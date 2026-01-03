0d6aa91444c2e140a6a173b8160f57cd05c30545
alysonoliveira-qa <alyson.oliveira3d@gmail.com>
2026-01-03 20:19:34 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,48 +1,64 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+import MyInfo from '../pages/myInfo.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+const myInfo = new MyInfo()
 
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton:"[type= 'submit']",
-    selectionTitleTopBar:".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: " .orangehrm-dashboard-grid",
-    wrongCredentialAlert:"[role='alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-    firstNameField: "[name='firstName']",
-    middleNameField: "[name='middleName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-mm-dd']",
-    dateCloseButton:".--close",
-    submitButton:"[type='submit']"
+    
+    // selectionTitleTopBar:".oxd-topbar-header-breadcrumb-module",
+    // dashboardGrid: " .orangehrm-dashboard-grid",
+    // 
+    // firstNameField: "[name='firstName']",
+    // middleNameField: "[name='middleName']",
+    // lastNameField: "[name='lastName']",
+    // genericField: ".oxd-input--active",
+    // dateField: "[placeholder='yyyy-mm-dd']",
+    // dateCloseButton:".--close",
+    // submitButton:"[type='submit']",
+    // listSelectMenu:".oxd-select-wrapper",
+    // selectCountry: ".oxd-select-dropdown > :nth-child(27)",
+    // selectSocialStatus: " .oxd-select-dropdown > :nth-child(3)",
   }
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.middleNameField).clear().type('Middle Name')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest')
-    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('DriverLicenseTest')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericField).eq(8).clear().type('SsNumberTest')
-    cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest')
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Sucessfully Updated')
-    cy.get(' .oxd-toast-close')
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+    myInfo.accessGenericField()
+    // cy.get(selectorsList.dashboardGrid)
+    // cy.get(selectorsList.myInfoButton).click()
+    // cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    // cy.get(selectorsList.middleNameField).clear().type('Middle Name')
+    // cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    // cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest')
+    // cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
+    // cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    // cy.get(selectorsList.genericField).eq(6).clear().type('DriverLicenseTest')
+    // cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')
+    // cy.get(selectorsList.dateCloseButton).click()
+    // cy.get(selectorsList.genericField).eq(8).clear().type('SsNumberTest')
+    // cy.get(selectorsList.genericField).eq(9).clear().type('sinNumberTest')
+    // cy.get(selectorsList.listSelectMenu).eq(0).click()
+    // cy.get(selectorsList.selectCountry).click()
+    // cy.get(selectorsList.listSelectMenu).eq(1).click()
+    // cy.get(selectorsList.selectSocialStatus).click()
+    // cy.get(selectorsList.submitButton).eq(0).click()
+    // cy.get('body').should('contain', 'Sucessfully Updated')
+    // cy.get(' .oxd-toast-close')
   })
+
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
